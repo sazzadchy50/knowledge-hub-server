159a02aa1a4339b0b0a9bc775e29f6f0d26b2284
sazzadchy50 <sazzadchy50@gmail.com>
2023-11-08 02:17:32 +0600

    update page created

--- index.js
+++ index.js
@@ -130,7 +130,10 @@ async function run() {
      
       res.send(result)   
     })
-    
+    app.patch("/api/v1/allBlog/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
