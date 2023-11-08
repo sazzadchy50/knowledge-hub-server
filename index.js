2d85dfe83241277bbbafa7dda959d1fda955b449
sazzadchy50 <sazzadchy50@gmail.com>
2023-11-08 19:57:18 +0600

    deploy to vercel

--- index.js
+++ index.js
@@ -172,13 +172,13 @@ async function run() {
       res.send(result)
 
     })
-    
+    g
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
-      
+
     );
     
   } finally {
